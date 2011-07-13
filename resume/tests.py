4f0cdb8fc7a0713183337de0e9e57af2d08e1edc
Randall Degges <rdegges@gmail.com>
2011-07-12 21:19:21 -0700

    Adding test case for git.

--- resume/tests.py
+++ resume/tests.py
@@ -11,5 +11,8 @@ class CompanyTestCase(TestCase):
     def setUp(self):
         self.company = Company()
 
+    def test_uses_git(self):
+        self.assertTrue('git' in self.company.dvcs)
+
     def tearDown(self):
         del self.company
