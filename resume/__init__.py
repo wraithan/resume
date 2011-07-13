733635dc3bf8fad7acdf8f9165bc43aa6f6b3364
Randall Degges <rdegges@gmail.com>
2011-07-12 20:16:53 -0700

    Adding docstring / metadata to package.

---
+++ resume/__init__.py
@@ -0,0 +1,21 @@
+"""Randall Degges - pythonista
+
+Are you looking for a good python programmer?
+=============================================
+
+* Someone who *understands* the need to have unit tests in their code.
+* Someone who enjoys reading technical books on their free time.
+* Someone who loves to write documentation along with their code.
+* Someone who participates in local programmer events.
+* Someone who builds and contributes to open source projects.
+* Someone who writes 1-day webapps on their weekends.
+
+If so--
+
+Continue onwards, and inspect the rest of the package!
+
+-Randall
+"""
+
+__author__ = 'Randall Degges'
+__email__ = 'rdegges@gmail.com'
